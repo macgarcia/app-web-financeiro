1feea208b5de30320404ae1b0745fa2862828b3f
macgarcia <marquim.iron@gmail.com>
2023-09-25 13:25:19 -0300

    Correção no processo de fechamento mensal.  - Mudança nos menus.  - Correção no processo de fechamento mensal.  - Correção na recuperação do valor do mes de fechamento. Marcos Garcia

--- src/main/java/com/github/macgarcia/web/beans/FechamentoMensalBean.java
+++ src/main/java/com/github/macgarcia/web/beans/FechamentoMensalBean.java
@@ -22,55 +22,54 @@ import com.github.macgarcia.web.repository.CalculoMensalRepository;
 public class FechamentoMensalBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Inject
 	private CalculoMensalRepository calculoMensalRepository;
-	
+
 	private List<CalculoMensal> calculos;
-	private Mes mesSelecionado = Mes.getMesComDigito(LocalDate.now().getMonthValue() -1);
+	private Mes mesFechmaento = Mes.getMesComDigito(LocalDate.now().getMonthValue() - 1);
 	private Double valorRendaMensal;
-	
+
 	@PostConstruct
 	public void init() {
 		setCalculos();
 	}
-	
+
 	private void setCalculos() {
-		this.calculos = calculoMensalRepository
-				.getCalculos();
+		this.calculos = calculoMensalRepository.getCalculos();
 	}
-	
+
 	public String telaInicial() {
 		return "index?faces-redirect=true";
 	}
-	
-	/* Gets para a tela*/
+
+	/* Gets para a tela */
 	public Mes[] getMeses() {
 		return Mes.values();
 	}
-	
-	public Mes getMesSelecionado() {
-		return mesSelecionado;
+
+	public Mes getMesFechmaento() {
+		return mesFechmaento;
 	}
-	
-	public void setMesSelecionado(Mes mesSelecionado) {
-		this.mesSelecionado = mesSelecionado;
+
+	public void setMesFechmaento(Mes mesFechmaento) {
+		this.mesFechmaento = mesFechmaento;
 	}
-	
+
 	public List<CalculoMensal> getCalculos() {
 		return calculos;
 	}
-	
+
 	public void setValorRendaMensal(Double valorRendaMensal) {
 		this.valorRendaMensal = valorRendaMensal;
 	}
-	
+
 	public Double getValorRendaMensal() {
 		return valorRendaMensal;
 	}
 	/**/
-	
-	/* Comportamentos da tela*/
+
+	/* Comportamentos da tela */
 	public void desfazerFechamento(Integer idCalculo) {
 		final Map<String, Object> parametros = new HashMap<String, Object>();
 		parametros.put("id_calculo_mensal_p", idCalculo.intValue());
@@ -79,11 +78,11 @@ public class FechamentoMensalBean implements Serializable {
 			setCalculos();
 		}
 	}
-	
+
 	public void executarFechamentoMensal() {
 		if (!Objects.isNull(valorRendaMensal)) {
 			final Map<String, Object> parametros = new HashMap<String, Object>();
-			parametros.put("mes_selecionado_p", mesSelecionado.name());
+			parametros.put("mes_selecionado_p", mesFechmaento.name());
 			parametros.put("valor_saldo_mensal_p", valorRendaMensal.doubleValue());
 			boolean processou = calculoMensalRepository.executeProcedure(ProcessosArmazenados.PROCESSAR_FECHAMENTO_MES, parametros);
 			if (processou) {
